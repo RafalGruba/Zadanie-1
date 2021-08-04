ff5d62e4f47b7623b7f3091cc3cf14a2d245d1db
RafalGruba <66263211+RafalGruba@users.noreply.github.com>
2021-08-04 15:39:04 +0200

    final

--- Zadanie 1/Assets/Player.cs
+++ Zadanie 1/Assets/Player.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     //config
-    [SerializeField] float crouchSpeed = 5f;
+    [Header("max move speed + jump force")]
+    [SerializeField] float crouchSpeed = 2f;
     [SerializeField] float walkSpeed = 15f;
     [SerializeField] float runSpeed = 25f;
-    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float jumpForce = 5f;
+    [Header("acceleration and brake")]
+    [SerializeField] float walkTimeZeroToMax = 2.5f;
+    [SerializeField] float runTimeZeroToMax = 2.5f;
+    [SerializeField] float brakeForce = 30f;
+    float timeBrakeToZero = 2f;
+    float walkAccelRatePerSec;
+    float runAccelRatePerSec;
+    float brakeRatePerSec;
+    float forwardVelocity;
 
     //states
-    public bool grounded = false;
+    bool grounded = false;
     bool isCrouching;
     bool isWalking;
     bool isRunning;
@@ -25,6 +35,10 @@ public class Player : MonoBehaviour
     {
         myRigidBody = GetComponent<Rigidbody>();
         myAnimator = GetComponent<Animator>();
+        walkAccelRatePerSec = walkSpeed / walkTimeZeroToMax;
+        runAccelRatePerSec = runSpeed / runTimeZeroToMax;
+        brakeRatePerSec = -brakeForce / timeBrakeToZero;
+        forwardVelocity = 0f;
     }
 
     private void FixedUpdate()
@@ -37,40 +51,37 @@ public class Player : MonoBehaviour
     private void Movement()
     {
         float controlThrow = Input.GetAxis("Vertical");
+        controlThrow = Mathf.Abs(controlThrow);
         if (controlThrow != 0 && Input.GetKey(KeyCode.LeftShift))
         {
-            Vector3 playerVelocity = new Vector3(myRigidBody.velocity.x, myRigidBody.velocity.y, Mathf.Abs(controlThrow * runSpeed));
-            myRigidBody.velocity = playerVelocity;
-            isCrouching = false;
-            isWalking = false;
-            isStanding = false;
-            isRunning = true;
+            forwardVelocity += walkAccelRatePerSec * Time.deltaTime;
+            forwardVelocity = Mathf.Min(forwardVelocity, runSpeed);
+            myRigidBody.velocity = transform.forward * (controlThrow * forwardVelocity);
+            isCrouching = false; isWalking = false; isStanding = false; isRunning = true;
         }
         else if (controlThrow != 0 && Input.GetKey(KeyCode.LeftControl))
         {
-            Vector3 playerVelocity = new Vector3(myRigidBody.velocity.x, myRigidBody.velocity.y, Mathf.Abs(controlThrow * crouchSpeed));
-            myRigidBody.velocity = playerVelocity;
-            isCrouching = true;
-            isWalking = false;
-            isStanding = false;
-            isRunning = false;
+            myRigidBody.velocity = transform.forward * (controlThrow * crouchSpeed);
+            isCrouching = true; isWalking = false; isStanding = false; isRunning = false;
         }
         else if (controlThrow != 0)
         {
-            Vector3 playerVelocity = new Vector3(myRigidBody.velocity.x, myRigidBody.velocity.y, Mathf.Abs(controlThrow * walkSpeed));
-            myRigidBody.velocity = playerVelocity;
-            isCrouching = false;
-            isWalking = true;
-            isStanding = false;
-            isRunning = false;
+            forwardVelocity += runAccelRatePerSec * Time.deltaTime;
+            forwardVelocity = Mathf.Min(forwardVelocity, walkSpeed);
+            myRigidBody.velocity = transform.forward * (controlThrow * forwardVelocity);
+            isCrouching = false; isWalking = true; isStanding = false; isRunning = false;
         }
         else
         {
-            isCrouching = false;
-            isWalking = false;
-            isStanding = true;
-            isRunning = false;
+            isCrouching = false; isWalking = false; isStanding = true; isRunning = false;
         }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            forwardVelocity += brakeRatePerSec * Time.deltaTime;
+            forwardVelocity = Mathf.Max(forwardVelocity, 0);
+            myRigidBody.velocity = transform.forward * (controlThrow * forwardVelocity);
+        }
+        forwardVelocity = myRigidBody.velocity.magnitude;
         // Set Animator Parameters:
         myAnimator.SetBool("isCrouching", isCrouching);
         myAnimator.SetBool("isWalking", isWalking);
@@ -87,7 +98,7 @@ public class Player : MonoBehaviour
         if (grounded && Input.GetButton("Jump"))
         {
             grounded = false;
-            Vector3 jumpVelocity = new Vector3(0f, jumpSpeed);
+            Vector3 jumpVelocity = new Vector3(0f, jumpForce);
             myRigidBody.velocity += jumpVelocity;
             myAnimator.SetTrigger("hasJumped");
         }
